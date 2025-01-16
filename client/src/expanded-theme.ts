c554e6ffe2c7f8a6e445720340fe2d5292af91bb
Edgar <edlyl619@gmail.com>
2025-01-17 00:32:33 +0800

    Update: expanded theme

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,10 +1,12 @@
+// eslint-disable-next-line
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
-  interface Palette {
+  interface PaletteColor {
     [key: number]: string;
   }
-  interface Palette{
+
+  interface Palette {
     tertiary: PaletteColor;
   }
 }
